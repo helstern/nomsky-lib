6275bd374386a365eede6e33c365ff95899d0f6d
Radu Helstern <radu.helstern@gmail.com>
2014-11-24 02:12:00 +0000

    modify eliminate groups visitor to use utility classes

--- src/Grammar/Converters/EliminateGroupsVisitor.php
+++ src/Grammar/Converters/EliminateGroupsVisitor.php
@@ -2,8 +2,6 @@
 
 use Helstern\Nomsky\Grammar\Expressions\Alternation;
 use Helstern\Nomsky\Grammar\Expressions\Expression;
-use Helstern\Nomsky\Grammar\Expressions\ExpressionAggregate;
-use Helstern\Nomsky\Grammar\Expressions\ExpressionIterable;
 use Helstern\Nomsky\Grammar\Expressions\Group;
 use Helstern\Nomsky\Grammar\Expressions\Sequence;
 
@@ -14,15 +12,19 @@ class EliminateGroupsVisitor extends AbstractErrorTriggeringVisitor implements H
     /** @var Expression */
     protected $root;
 
-    /** @var Expression[] */
-    protected $expressions;
+    /** @var array|Combinator[] */
+    protected $stackOfCombinators = array();
 
     /** @var array[] */
-    protected $stackOfChildren;
+    protected $stackOfChildExpressions = array();
 
-    /** @var CombinatorAdapterForExpressionGroup[] */
-    protected $stackOfCombinators;
+    /** @var array[] */
+    protected $stackOfChildGroupCombinators = array();
 
+    /** @var array|ExpressionStackPushStrategy  */
+    protected $stackPushStrategiesStack = array();
+
+    /** @var bool */
     protected $parentIsGroup = false;
 
     /**
@@ -35,175 +37,106 @@ class EliminateGroupsVisitor extends AbstractErrorTriggeringVisitor implements H
 
     /**
      * @param Expression $e
+     * @param Combinator $c
      */
-    protected function setAsRootOrAddToStackOfChildren(Expression $e)
+    protected  function onBeforeStartVisitExpressionIterable(Expression $e, Combinator $c)
     {
-        if (empty($this->stackOfChildren)) {
-            $this->root = $e;
+        $stackPushStrategy = new ExpressionStackPushStrategy($this->parentIsGroup);
+        array_push($this->stackPushStrategiesStack, $stackPushStrategy);
+
+        array_push($this->stackOfCombinators, $c);
+
+        $this->stackOfChildExpressions[]    = array();
+        $this->stackOfChildGroupCombinators[] = array();
+    }
+
+    /**
+     * @param Expression $e
+     */
+    protected function onAfterEndVisitExpressionIterable(Expression $e)
+    {
+        $stackPushStrategy      = array_pop($this->stackPushStrategiesStack);
+        /** @var Combinator $c */
+        $c                      = array_pop($this->stackOfCombinators);
+
+        /** @var array|Expression[] $children */
+        $children               = array_pop($this->stackOfChildExpressions);
+        /** @var array|Combinator[] $childGroupCombinators */
+        $childGroupCombinators  = array_pop($this->stackOfChildGroupCombinators);
+
+        if (empty($childGroupCombinators)) {
+            $childExpression = $e;
+            $childCombinator = $c;
         } else {
-            $this->addToStackOfChildren($e);
-        }
-    }
+            $builder = new ExpressionCombinatorBuilder();
+            $endVisitExpressionHandler = new ExpressionGroupsRemover($c);
+            $endVisitExpressionHandler->removeGroups($children, $childGroupCombinators, $builder);
 
-    /**
-     * @param Expression $e
-     */
-    protected function addToStackOfChildren(Expression $e)
-    {
-        /** @var Expression[]|array $parentChildren */
-        $parentChildren = array_pop($this->stackOfChildren);
-        array_push($parentChildren, $e);
-        array_push($this->stackOfChildren, $parentChildren);
-    }
-
-    /**
-     * @param Expression $e
-     * @param array $items
-     * @throws \Exception
-     */
-    protected function onAfterEndVisitExpressionIterable(Expression $e, array $items)
-    {
-        /** @var array $head */
-        $head = null;
-        $tail = $items;
-        do {
-            $processed  = 0;
-            $remaining  = count($tail);
-            $groupFound = false;
-            $head       = array();
-
-            do {
-                $item = array_shift($tail);
-                $remaining--;
-
-                if (is_array($item)) {
-                    $groupFound = true;
-                    if ($processed === 0) {
-                        $tail = $this->eliminateGroup($item, $tail);
-                   } elseif ($remaining === 0) {
-                        $tail = $this->eliminateGroup($head, $item);
-                    } else {
-                        $head = $this->eliminateGroup($head, $item);
-                        $tail = array_merge($head, $tail);
-                    }
-                } else {
-                    $head[] = $item;
-                }
-
-                $processed++;
-            } while ($remaining > 0 && $groupFound == false );
-        } while($groupFound == true);
-
-        /** @var CombinatorAdapterForExpressionGroup $combinatorAdapter */
-        $combinatorAdapter = array_pop($this->stackOfCombinators);
-        if ($combinatorAdapter->expressionIsChildOfGroup()) {
-            array_push($this->stackOfCombinators, $combinatorAdapter);
-            array_push($this->stackOfChildren, $head);
-            return;
+            $childExpression = $builder->build();
+            $childCombinator = $builder->getCombinator();
         }
 
-        $combinator = $combinatorAdapter->getCombinator();
-        $expression = $combinator->createExpression($head);
-        $this->setAsRootOrAddToStackOfChildren($expression);
-    }
-
-    /**
-     * @param array $first
-     * @param array $second
-     * @return array|null whether the operation eliminated a group or nit
-     */
-    protected function eliminateGroup(array $first, array $second)
-    {
-        if (empty($first)) {
-            return null;
+        $newChildStack = $stackPushStrategy->pushChild($childExpression, $this->stackOfChildExpressions);
+        if (is_null($newChildStack)) {
+            $this->root = $childExpression;
+        } else {
+            $this->stackOfChildExpressions = $newChildStack;
         }
 
-        /** @var CombinatorAdapterForExpressionGroup $secondCombinatorAdapter */
-        $secondCombinatorAdapter   = array_pop($this->stackOfCombinators);
-        /** @var CombinatorAdapterForExpressionGroup $firstCombinatorAdapter */
-        $firstCombinatorAdapter    = array_pop($this->stackOfCombinators);
-        $combination = $firstCombinatorAdapter->createCombination($secondCombinatorAdapter);
-
-        $combinator         = $combination->getCombinator();
-        $combinatorAdapter  = $this->adaptCombinator($combinator, $firstCombinatorAdapter->expressionIsChildOfGroup());
-        array_push($this->stackOfCombinators, $combinatorAdapter);
-
-        $children           = $combination->combine($first, $second);
-        return $children;
-    }
-
-    /**
-     * @param Combinator $combinator
-     * @param bool $groupVisitedLast
-     * @return CombinatorAdapterForExpressionGroup
-     */
-    protected function adaptCombinator(Combinator $combinator, $groupVisitedLast)
-    {
-        $adapter = new CombinatorAdapterForExpressionGroup($combinator, $groupVisitedLast);
-        return $adapter;
+        $newCombinatorStack = $stackPushStrategy->pushCombinator($childCombinator, $this->stackOfChildGroupCombinators);
+        if (!is_null($newCombinatorStack)) {
+            $this->stackOfChildGroupCombinators = $newCombinatorStack;
+        }
     }
 
     public function startVisitAlternation(Alternation $expression)
     {
-        if (!$this->parentIsGroup) {
-            $this->stackOfChildren[]    = array();
-        }
-
         $combinator                 = new CombinatorForAlternation();
-        $adapter                    = $this->adaptCombinator($combinator, $this->parentIsGroup);
-        $this->stackOfCombinators[] = $adapter;
+        $this->onBeforeStartVisitExpressionIterable($expression, $combinator);
 
-        $this->parentIsGroup = false;
+        if ($this->parentIsGroup) {
+            $this->parentIsGroup        = false;
+        }
 
         return true;
     }
 
     public function endVisitAlternation(Alternation $expression)
     {
-        $items          = array_pop($this->stackOfChildren);
-        $this->onAfterEndVisitExpressionIterable($expression, $items);
+        $this->onAfterEndVisitExpressionIterable($expression);
 
         return true;
     }
 
     public function startVisitSequence(Sequence $expression)
     {
-        if (!$this->parentIsGroup) {
-            $this->stackOfChildren[] = array();
-        }
-
         $combinator                 = new CombinatorForSequence();
-        $adapter                    = $this->adaptCombinator($combinator, $this->parentIsGroup);
-        $this->stackOfCombinators[] = $adapter;
+        $this->onBeforeStartVisitExpressionIterable($expression, $combinator);
 
-        $this->parentIsGroup = false;
+        if ($this->parentIsGroup) {
+            $this->parentIsGroup = false;
+        }
 
         return true;
     }
 
     public function endVisitSequence(Sequence $expression)
     {
-        $items      = array_pop($this->stackOfChildren);
-        $this->onAfterEndVisitExpressionIterable($expression, $items);
+        $this->onAfterEndVisitExpressionIterable($expression);
 
         return true;
     }
 
     public function startVisitGroup(Group $expression)
     {
-        $this->stackOfChildren[] = array();
-        $this->parentIsGroup = true;
+        $this->parentIsGroup        = true;
 
         return true;
     }
 
     public function endVisitGroup(Group $expression)
     {
-        $groupItems = array_pop($this->stackOfChildren);
-        $parentChildren = array_pop($this->stackOfChildren);
-
-        array_push($parentChildren, $groupItems);
-        array_push($this->stackOfChildren, $parentChildren);
+        //transfer the children of the group to the group's parent
 
         return true;
     }
@@ -211,9 +144,9 @@ class EliminateGroupsVisitor extends AbstractErrorTriggeringVisitor implements H
     public function visitExpression(Expression $expression)
     {
         /** @var array $lastListOfSymbols */
-        $lastListOfSymbols = array_pop($this->stackOfChildren);
+        $lastListOfSymbols = array_pop($this->stackOfChildExpressions);
         $lastListOfSymbols[] = $expression;
-        array_push($this->stackOfChildren, $lastListOfSymbols);
+        array_push($this->stackOfChildExpressions, $lastListOfSymbols);
 
         return true;
     }
@@ -230,46 +163,3 @@ class EliminateGroupsVisitor extends AbstractErrorTriggeringVisitor implements H
         return $warningMessage;
     }
 }
-
-
-/**
-
-(a | b | c | (1 | 2 | 3))
-a
-b
-c
-
-
-a b c (1 | 2 | 3)
-
-
-Alternation
-Sequence
-
-
-array(a b c)
-
-array(a b c 1, a b c 2, a b c 3)
-
-
-(a | b ) (1 | 2 | 3 | (x | y | z) | 4)
-
-
-array(a, b)
-array(1, 2, 3)
-array()
-array(x, y, z)
-
-array
-
-(a | b ) (1 | 2 | 3 | p q r (x | y | z) | 4)
-(a | b ) (1 | 2 | 3 | (p q r x | p q r y | p q r z) | 4)
-(a | b ) (1 | 2 | 3 | p q r x | p q r y | p q r z | 4)
-
-
-
-p q r (x | y | {1 | 2})
-
-p q r (x | y | 1 | 2
-
-**/
