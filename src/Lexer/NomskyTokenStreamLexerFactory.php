81331418d8b390ea0603cc8277a98deec78c6095
Radu Helstern <radu.helstern@gmail.com>
2015-02-19 03:08:15 +0000

    add token stream list matcher in lexer namespace refactor lexer factory

---
+++ src/Lexer/NomskyTokenStreamLexerFactory.php
@@ -0,0 +1,83 @@
+<?php namespace Helstern\Nomsky\Lexer;
+
+use Helstern\Nomsky\Lexer\TokenStream\LongestMatchListMatcher;
+use Helstern\Nomsky\Text\TextSource;
+use Helstern\Nomsky\Text\TokenSource\FileSource\FileSource;
+use Helstern\Nomsky\TextMatch\RegexPatternBuilder;
+
+use Helstern\Nomsky\Tokens\TokenMatch\AnchoredPcreMatcher;
+use Helstern\Nomsky\Tokens\TokenPattern\RegexTokenPattern;
+use Helstern\Nomsky\Tokens\TokenStream\MatcherListTokenStream;
+
+class NomskyTokenStreamLexerFactory
+{
+    /**
+     * @param $filePath
+     * @return TokenStreamLexer
+     */
+    public function fromFile($filePath)
+    {
+        $fileDescriptor = new \SplFileInfo($filePath);
+
+        $source = new FileSource($fileDescriptor);
+        $tokenPatterns = NomskyTokenPatterns::regexPatterns();
+        $tokenStream = $this->createTokenStream($source, $tokenPatterns);
+
+        $lexer = new TokenStreamLexer($tokenStream);
+
+        return $lexer;
+    }
+
+    /**
+     * @param TextSource $source
+     * @param array|RegexTokenPattern[] $tokenPatterns
+     * @return MatcherListTokenStream
+     */
+    protected function createTokenStream(TextSource $source, array $tokenPatterns)
+    {
+        $reader = $source->createReader();
+        $matcherListAdapter = $this->createLongestMatchListMatcher($tokenPatterns);
+
+        $tokenStream = new MatcherListTokenStream($reader, $matcherListAdapter);
+        return $tokenStream;
+    }
+
+    /**
+     * @param array|RegexTokenPattern[] $tokenPatterns
+     * @return LongestMatchListMatcher
+     */
+    protected function createLongestMatchListMatcher(array $tokenPatterns)
+    {
+        $matchers = [];
+        foreach ($tokenPatterns as $pattern) {
+            $matchers[] = $this->createPcreMatcher($pattern);
+        }
+
+        $listMatcher = new LongestMatchListMatcher($matchers);
+        return $listMatcher;
+    }
+
+    /**
+     * @param array|RegexTokenPattern[] $tokenPatterns
+     * @return string
+     */
+    protected function createRegexPattern(array $tokenPatterns)
+    {
+        $regexBuilder = new RegexPatternBuilder();
+        foreach ($tokenPatterns as $tokenPattern) {
+            $regexBuilder->addNamedPattern($tokenPattern->getTokenType(), $tokenPattern->getTokenPattern());
+        }
+
+        $pattern = $regexBuilder->build();
+        return $pattern;
+    }
+
+    /**
+     * @param RegexTokenPattern $pattern
+     * @return AnchoredPcreMatcher
+     */
+    protected function createPcreMatcher(RegexTokenPattern $pattern)
+    {
+        return new AnchoredPcreMatcher($pattern);
+    }
+}
---
+++ src/Lexer/TokenStream/LongestMatchListMatcher.php
@@ -0,0 +1,104 @@
+<?php namespace Helstern\Nomsky\Lexer\TokenStream;
+
+use Helstern\Nomsky\Text\TextMatch;
+use Helstern\Nomsky\Text\TextReader;
+use Helstern\Nomsky\Text\String\StringMatch;
+
+use Helstern\Nomsky\Tokens\TokenMatch\TokenMatch;
+use Helstern\Nomsky\Tokens\TokenMatch\TokenStringMatcher;
+use Helstern\Nomsky\Tokens\TokenStream\TokenStringMatcherListAdapter;
+
+class LongestMatchListMatcher implements TokenStringMatcherListAdapter
+{
+    /** @var array|TokenStringMatcher[] */
+    protected $tokenMatchersList;
+
+    /**
+     * @param array|TokenStringMatcher[] $tokenMatchersList
+     */
+    public function __construct(array $tokenMatchersList)
+    {
+        $this->tokenMatchersList = $tokenMatchersList;
+    }
+
+    /**
+     * @return array|TokenStringMatcher[]
+     */
+    public function getTokenMatchersList()
+    {
+        return $this->tokenMatchersList;
+    }
+
+    /**
+     * @param TextReader $reader
+     * @return TokenMatch|null
+     */
+    public function match(TextReader $reader)
+    {
+        $oneCharacterAhead = $reader->readCharacter();
+        if (is_null($oneCharacterAhead)) {
+            return null;
+        }
+
+        /** @var StringMatch $longestMatch */
+        $longestMatch = new StringMatch('');
+        /** @var array|TokenMatch[] $equallyLongTokenMatches */
+        $equallyLongTokenMatches = null;
+
+        foreach ($this->tokenMatchersList as $matcher) {
+            $nextMatch = $reader->readTextMatch($matcher);
+            if (is_null($nextMatch)) {
+                continue;
+            }
+
+            $longestMatch = $this->chooseLongestMatch($nextMatch, $longestMatch);
+            if ($longestMatch === $nextMatch) { //new longest match
+                $equallyLongTokenMatches = [];
+            } elseif (is_null($longestMatch)) { //same length match
+                $longestMatch = $nextMatch;
+            } else { //shorter match
+                continue;
+            }
+
+            $tokenMatch = $this->createTokenMatch($nextMatch, $matcher);
+            $equallyLongTokenMatches[] = $tokenMatch;
+        }
+
+        if (0 == $longestMatch->getCharLength()) {
+            return null;
+        }
+
+        //disregard the fact that there could be more than one longest matches
+        $tokenMatch = reset($equallyLongTokenMatches);
+        return $tokenMatch;
+    }
+
+    /**
+     * @param TextMatch $textMatch
+     * @param TokenStringMatcher $matchedBy
+     * @return TokenMatch
+     */
+    protected function createTokenMatch(TextMatch $textMatch, TokenStringMatcher $matchedBy)
+    {
+        $tokenPattern = $matchedBy->getTokenPattern();
+        return new TokenMatch($textMatch, $tokenPattern);
+    }
+
+    /**
+     * @param TextMatch $match
+     * @param TextMatch $longestMatch
+     * @return TextMatch|null
+     */
+    protected function chooseLongestMatch(TextMatch $match, TextMatch $longestMatch)
+    {
+        if ($match->getCharLength() < $longestMatch->getCharLength()) {
+            return $longestMatch;
+        }
+
+        if ($match->getCharLength() == $longestMatch->getCharLength()) {
+            return null;
+        }
+
+        return $match;
+    }
+}
