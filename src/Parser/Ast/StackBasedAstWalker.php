89e02608258963f867bab6ed55a4d5090cf3f189
Radu Helstern <radu.helstern@gmail.com>
2015-03-12 02:45:26 +0000

    fix stack based ast walker

--- src/Parser/Ast/StackBasedAstWalker.php
+++ src/Parser/Ast/StackBasedAstWalker.php
@@ -2,21 +2,14 @@
 
 class StackBasedAstWalker implements AstWalker
 {
-    /** @var AstNodeVisitorProvider */
-    protected $astNodeVisitorProvider;
-
     /** @var AstNodeWalkStrategy */
     protected $childrenWalkStrategy;
 
     /**
-     * @param AstNodeVisitorProvider $visitActionDispatcher
      * @param AstNodeWalkStrategy $childrenWalkStrategy
      */
-    public function __construct(
-        AstNodeVisitorProvider $visitActionDispatcher,
-        AstNodeWalkStrategy $childrenWalkStrategy
-    ) {
-        $this->astNodeVisitorProvider = $visitActionDispatcher;
+    public function __construct(AstNodeWalkStrategy $childrenWalkStrategy)
+    {
         $this->childrenWalkStrategy = $childrenWalkStrategy;
     }
 
@@ -65,63 +58,11 @@ class StackBasedAstWalker implements AstWalker
     }
 
     /**
-     * @param AstNode $astNode
-     * @return boolean false when the walk ended pre-maturely
-     */
-    public function walkWithDispatch(AstNode $astNode)
-    {
-        /** @var AstNode[]|VisitAction[] $stackOfExpressions */
-        $initialNode = $astNode;
-        $stackOfVisits = array($initialNode);
-        $stackHeight = 1;
-
-        do {
-            $nodeOrVisitAction = array_pop($stackOfVisits);
-            $stackHeight--;
-
-            if ($nodeOrVisitAction instanceof VisitAction) {
-                $this->executeVisitAction($nodeOrVisitAction);
-                continue;
-            }
-
-            $astNode = $nodeOrVisitAction;
-            $parentNextVisitAction = $this->dispatchVisitAction($astNode);
-
-            /** @var \SplDoublyLinkedList $visitList */
-            $visitList = null;
-            if (is_null($parentNextVisitAction)) {
-                $visitList = $this->determineListOfChildrenVisits($astNode);
-            } else {
-                $visitList = $this->determineListOfVisits($astNode, $parentNextVisitAction);
-            }
-
-            //reverse push the children linked list, such that the first child is last in $stackOfExpressions
-            foreach ($visitList as $nextVisit) {
-                array_push($stackOfVisits, $nextVisit);
-                $stackHeight++;
-            }
-
-        } while ($stackHeight > 0);
-
-        return true;
-    }
-
-    /**
      * @param VisitAction $visitAction
      */
     protected function executeVisitAction(VisitAction $visitAction)
     {
         $visitAction->execute();
-    }
-
-    /**
-     * @param AstNode $astNode
-     * @return VisitAction|null
-     */
-    protected function dispatchVisitAction(AstNode $astNode)
-    {
-        $visitAction = $this->astNodeVisitorProvider->dispatchVisit($astNode);
-        return $visitAction;
     }
 
     /**
