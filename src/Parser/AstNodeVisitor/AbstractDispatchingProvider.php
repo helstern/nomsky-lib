32b2a6ef8e64c28ec4aff185df4546b8488593e2
Radu Helstern <radu.helstern@gmail.com>
2015-03-15 01:07:39 +0000

    add abstract dispatching visitor provider

---
+++ src/Parser/AstNodeVisitor/AbstractDispatchingProvider.php
@@ -0,0 +1,35 @@
+<?php namespace Helstern\Nomsky\Parser\AstNodeVisitor;
+
+use Helstern\Nomsky\Dispatcher\DoubleDispatcher;
+use Helstern\Nomsky\Parser\Ast\AstNode;
+use Helstern\Nomsky\Parser\Ast\AstNodeVisitor;
+use Helstern\Nomsky\Parser\Ast\AstNodeVisitorProvider;
+
+abstract class AbstractDispatchingProvider implements AstNodeVisitorProvider
+{
+    /**
+     * @param AstNode $node
+     * @return AstNodeVisitor
+     */
+    public function getVisitor(AstNode $node)
+    {
+        $dispatcherBuilder = new VisitorProviderDispatcherBuilder();
+        $dispatcher = $node->buildDoubleDispatcher($dispatcherBuilder);
+
+        $visitor = $this->dispatch($dispatcher, $node);
+        return $visitor;
+    }
+
+    /**
+     * @param DoubleDispatcher $dispatcher
+     * @param object $argument
+     * @return AstNodeVisitor
+     */
+    protected function dispatch(DoubleDispatcher $dispatcher, $argument)
+    {
+        /** @var AstNodeVisitor $visitor */
+        $visitor = $dispatcher->dispatch($this, $argument);
+        return $visitor;
+    }
+
+}
