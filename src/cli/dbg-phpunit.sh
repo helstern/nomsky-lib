f069555204669b24413b27fb2be89c49809497e4
Radu Helstern <radu.helstern@gmail.com>
2015-03-18 22:03:11 +0000

    add cli module

---
+++ src/cli/dbg-phpunit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# directories
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR="$(dirname $SOURCE_DIR)"
+
+#absolute path to phpunit
+PHPUNIT="$PROJECT_DIR/vendor/bin/phpunit"
+
+# default phpunit options
+PHPUNITDEFAULTS_CONFIGURATION="phpunit.local.xml"
+
+# xdebug configuration
+PHP_IDE_SERVER_NAME='dev-php'
+
+PHPUNITARGS="$@"
+
+if ! [[ "$PHPUNITARGS" =~ '--help' ]] && ! [[ "$PHPUNITARGS" =~ '--configuration|-c' ]]; then
+    PHPUNITARGS="--configuration=$PHPUNITDEFAULTS_CONFIGURATION $PHPUNITARGS"
+fi
+
+export PHP_IDE_CONFIG="serverName=${PHP_IDE_SERVER_NAME}" XDEBUG_CONFIG="idekey=PHPSTORM"
+${PHPUNIT} ${PHPUNITARGS}
