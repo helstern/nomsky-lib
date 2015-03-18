554bff5fcdbe1f0cace9d2b9dbf910c7df161023
Radu Helstern <radu.helstern@gmail.com>
2015-03-18 23:08:33 +0000

    fix cli dir

--- src/cli/dbg-phpunit.sh
+++ src/cli/dbg-phpunit.sh
@@ -2,13 +2,22 @@
 
 # directories
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_DIR="$(dirname $SOURCE_DIR)"
+PROJECT_DIR="${SOURCE_DIR}"
+
+DEPTH=2
+until [ $DEPTH -le 0 ] ; do
+    let DEPTH-=1
+    PROJECT_DIR="$(dirname ${PROJECT_DIR})"
+done
 
 #absolute path to phpunit
-PHPUNIT="$PROJECT_DIR/vendor/bin/phpunit"
+PHPUNIT="$PROJECT_DIR/composer/bin/phpunit"
+
+#absolute path to configuration
+PHPUNIT_CONFIGURATION_DIR="$PROJECT_DIR/src/test/resources/"
 
 # default phpunit options
-PHPUNITDEFAULTS_CONFIGURATION="phpunit.local.xml"
+PHPUNIT_DEFAULT_CONFIGURATION_FILE="$PROJECT_DIR/src/test/resources/phpunit.local.xml"
 
 # xdebug configuration
 PHP_IDE_SERVER_NAME='dev-php'
@@ -16,7 +25,7 @@ PHP_IDE_SERVER_NAME='dev-php'
 PHPUNITARGS="$@"
 
 if ! [[ "$PHPUNITARGS" =~ '--help' ]] && ! [[ "$PHPUNITARGS" =~ '--configuration|-c' ]]; then
-    PHPUNITARGS="--configuration=$PHPUNITDEFAULTS_CONFIGURATION $PHPUNITARGS"
+    PHPUNITARGS="--configuration=$PHPUNIT_DEFAULT_CONFIGURATION_FILE $PHPUNITARGS"
 fi
 
 export PHP_IDE_CONFIG="serverName=${PHP_IDE_SERVER_NAME}" XDEBUG_CONFIG="idekey=PHPSTORM"
