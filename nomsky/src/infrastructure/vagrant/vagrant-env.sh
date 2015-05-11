c1e78142d3b8730ab216999e589f474b33b5cec4
radu helstern <radu.helstern@gmail.com>
2015-05-11 02:10:18 +0100

    fix vagrant loading

--- nomsky/src/infrastructure/vagrant/vagrant-env.sh
+++ nomsky/src/infrastructure/vagrant/vagrant-env.sh
@@ -5,6 +5,6 @@ PROJECT_DIR="$(cd "${CURRENT_DIR}/../../../";pwd)"
 
 # this is not needed
 # export VAGRANT_CWD=${PROJECT_DIR}/src/infrastructure/vagrant
-export VAGRANT_DEFAULT_PROVIDER=libvirt
+export VAGRANT_PROJECT_DIR=${PROJECT_DIR}
 export VAGRANT_DOTFILE_PATH=${PROJECT_DIR}/target/vagrant
 export VAGRANT_PROVIDER=libvirt
