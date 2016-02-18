f73d474fea7cc54635fa5ee4cd0d809e88c08562
radu helstern <radu.helstern@gmail.com>
2016-02-18 22:30:47 +0000

    rename test case

--- src/test/php/Grammars/Ebnf/Grammar/AstTranslatorTest.php
+++ src/test/php/Grammars/Ebnf/Grammar/AstTranslatorTest.php
@@ -1,12 +1,13 @@
 <?php namespace Helstern\Nomsky\Grammars\Ebnf\Grammar;
 
+use Helstern\Nomsky\Grammar\Conversions;
 use Helstern\Nomsky\Grammars\Ebnf\IsoEbnf\LexerFactory as IsoEbnfLexerFactory;
 use Helstern\Nomsky\Grammars\Ebnf\IsoEbnf\Parser as IsoEbnfParser;
 use Helstern\Nomsky\Parser\Errors\ParseAssertions;
 use Helstern\Nomsky\TestCase;
 use Helstern\Nomsky\Tokens\TokenPredicates;
 
-class GrammarTranslatorTest extends TestCase
+class AstTranslatorTest extends TestCase
 {
     public function testLogoGrammar()
     {
@@ -21,6 +22,24 @@ class GrammarTranslatorTest extends TestCase
         $grammar = $translator->translate($syntaxNode);
 
         $this->assertNotNull($grammar);
+    }
+
+    public function testBnfConversion()
+    {
+        $grammarFile = $this->getResourceFilePath('logo-simple.ebnf');
+        $lexer = (new IsoEbnfLexerFactory())->fromFile($grammarFile);
+
+        $assertions = new ParseAssertions(new TokenPredicates);
+        $parser = new IsoEbnfParser($assertions);
+
+        $syntaxNode = $parser->parse($lexer);
+        $translator = new AstTranslator();
+        $ebnfGrammar = $translator->translate($syntaxNode);
+
+        $conversions = new Conversions();
+        $bnfGrammar = $conversions->ebnfToBnf($ebnfGrammar);
+
+        $this->assertNotNull($bnfGrammar);
     }
 }
 
